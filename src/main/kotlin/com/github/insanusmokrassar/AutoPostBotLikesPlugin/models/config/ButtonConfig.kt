819115eb927e9a92f3abf8c39d3c3a8eec56a5a8
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2019-01-20 14:42:28 +0800

    serialization fixes

--- src/main/kotlin/com/github/insanusmokrassar/AutoPostBotLikesPlugin/models/config/ButtonConfig.kt
+++ src/main/kotlin/com/github/insanusmokrassar/AutoPostBotLikesPlugin/models/config/ButtonConfig.kt
@@ -1,5 +1,6 @@
 package com.github.insanusmokrassar.AutoPostBotLikesPlugin.models.config
 
+import kotlinx.serialization.Optional
 import kotlinx.serialization.Serializable
 
 private const val textIdentifier = "\$text"
@@ -7,10 +8,15 @@ private const val countIdentifier = "\$count"
 
 @Serializable
 data class ButtonConfig (
+    @Optional
     val text: String = "",
+    @Optional
     val id: String = "",
+    @Optional
     val format: String = "\$text \$count",
+    @Optional
     val positiveAnswer: AnswerConfig? = null,
+    @Optional
     val negativeAnswer: AnswerConfig? = null
 ) {
     fun format(count: Int): String {
