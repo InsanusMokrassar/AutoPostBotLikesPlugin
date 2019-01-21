7d7868233ea8ddefb79d656dbf844245b0e06852
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2019-01-21 16:10:00 +0800

    rewrite AdminsHolder to be synchronous on getting update operation

--- src/main/kotlin/com/github/insanusmokrassar/AutoPostBotLikesPlugin/utils/extensions/AdminsHolder.kt
+++ src/main/kotlin/com/github/insanusmokrassar/AutoPostBotLikesPlugin/utils/extensions/AdminsHolder.kt
@@ -1,12 +1,18 @@
 package com.github.insanusmokrassar.AutoPostBotLikesPlugin.utils.extensions
 
+import com.github.insanusmokrassar.AutoPostTelegramBot.utils.NewDefaultCoroutineScope
+import com.github.insanusmokrassar.AutoPostTelegramBot.utils.extensions.subscribe
 import com.github.insanusmokrassar.TelegramBotAPI.bot.RequestsExecutor
 import com.github.insanusmokrassar.TelegramBotAPI.requests.chat.get.GetChatAdministrators
 import com.github.insanusmokrassar.TelegramBotAPI.types.ChatId
-import com.github.insanusmokrassar.TelegramBotAPI.types.ChatMember.AdministratorChatMember
+import com.github.insanusmokrassar.TelegramBotAPI.types.ChatMember.*
 import com.github.insanusmokrassar.TelegramBotAPI.types.UserId
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
 import org.joda.time.DateTime
 import java.lang.ref.WeakReference
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.suspendCoroutine
 
 class AdminsHolder(
     private val botWR: WeakReference<RequestsExecutor>,
@@ -16,20 +22,30 @@ class AdminsHolder(
     private val admins: MutableList<AdministratorChatMember> = ArrayList()
     private val lastRequestTime = DateTime(0)
 
-    suspend fun contains(userId: UserId): Boolean {
-        if ((lastRequestTime + requestDelay).isBeforeNow) {
-            val adminsResponse = botWR.get() ?.execute(
-                GetChatAdministrators(
-                    chatId
+    private val adminsHolderScope = NewDefaultCoroutineScope(2)
+    private val adminsChangesChannel = Channel<Pair<UserId, Continuation<Boolean>>>(Channel.UNLIMITED)
+    private val job = adminsHolderScope.launch {
+        for (pair in adminsChangesChannel) {
+            if ((lastRequestTime + requestDelay).isBeforeNow) {
+                val adminsResponse = botWR.get() ?.execute(
+                    GetChatAdministrators(
+                        chatId
+                    )
+                ) ?: throw IllegalStateException("Bot was destroyed")
+                admins.clear()
+                admins.addAll(
+                    adminsResponse.mapNotNull {
+                        it.asChatMember as? AdministratorChatMember
+                    }
                 )
-            ) ?: throw IllegalStateException("Bot was destroyed")
-            admins.clear()
-            admins.addAll(
-                adminsResponse.mapNotNull {
-                    it.asChatMember as? AdministratorChatMember
-                }
-            )
+            }
+            pair.second.resumeWith(Result.success(admins.firstOrNull { it.user.id == pair.first } != null))
         }
-        return admins.firstOrNull { it.user.id == userId } != null
+    }
+
+    suspend fun contains(userId: UserId): Boolean {
+        return suspendCoroutine {
+            adminsHolderScope.launch { adminsChangesChannel.send(userId to it) }
+        }
     }
 }
